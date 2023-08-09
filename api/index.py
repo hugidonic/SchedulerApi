ed2f598d5a779e60dd85aa57d52bb000c1230a2b
Hugidonic <netvaca@ya.ru>
2023-08-09 12:50:54 +0300

    Schedule routes added

--- api/index.py
+++ api/index.py
@@ -2,12 +2,88 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# https://www.kstu.ru/www_Ggrid.jsp? d=2022-11-09&g=34326
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/schedule_info")
+async def get_schedule_info():
+    return {
+        "dayOfWeek": "Ср",
+        "typeOfWeek": "Нечет",
+        "date": "09.11.2022",
+    }
 
+@app.get("/subjects")
+async def get_schedule_info():
+    return [
+        None,
+        {
+            "title": "Основы информационной безопасности",
+            "shortTitle": "ОИБ",
+            "type": "Лекция",
+            "prepod": "Садыков А.М.",
+            "cabinet": "И-1-209",
+            "date": "1 сен - 1 янв"
+        },
+        None,
+        {
+            "title": "Информационные технологии в информационной безопасности",
+            "shortTitle": "ИТВИБ",
+            "type": "Лабораторная работа",
+            "prepod": "Богомолов В.А.",
+            "cabinet": "П-7",
+            "date": "1 сен - 4 дек"
+        },
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+        {
+            "title": "Теория вероятностей и математическая статистика",
+            "shortTitle": "ТВИМС",
+            "type": "Практика",
+            "prepod": "Хайруллин М.Х.",
+            "cabinet": "Д-104а",
+            "date": "31 окт - 1 янв"
+        },
+        None,
+        None,
+        None
+    ]
+
+@app.get("/full_schedule")
+async def get_schedule_info():
+    return {
+        "dayOfWeek": "Ср",
+        "typeOfWeek": "Нечет",
+        "date": "09.11.2022",
+        "subjects": [
+            None,
+            {
+                "title": "Основы информационной безопасности",
+                "shortTitle": "ОИБ",
+                "type": "Лекция",
+                "prepod": "Садыков А.М.",
+                "cabinet": "И-1-209",
+                "date": "1 сен - 1 янв"
+            },
+
+            None,
+            {
+                "title": "Информационные технологии в информационной безопасности",
+                "shortTitle": "ИТВИБ",
+                "type": "Лабораторная работа",
+                "prepod": "Богомолов В.А.",
+                "cabinet": "П-7",
+                "date": "1 сен - 4 дек"
+            },
+
+            {
+                "title": "Теория вероятностей и математическая статистика",
+                "shortTitle": "ТВИМС",
+                "type": "Практика",
+                "prepod": "Хайруллин М.Х.",
+                "cabinet": "Д-104а",
+                "date": "31 окт - 1 янв"
+            },
+            None,
+            None,
+            None
+        ]
+    }
