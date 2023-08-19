25fd1542e740f9de4801ea4903fecd63742c2ae9
Hugidonic <netvaca@ya.ru>
2023-08-19 13:50:42 +0300

    Json files with schedules

--- api/index.py
+++ api/index.py
@@ -1,89 +1,52 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import simplejson as json
+from datetime import datetime
 
 app = FastAPI()
+chet = "./api/json/chet/"
+nechet = "./api/json/nechet/"
 
-# https://www.kstu.ru/www_Ggrid.jsp? d=2022-11-09&g=34326
+# https://www.kstu.ru/www_Ggrid.jsp?x=www_GFgrid&d=2022-11-09&f=210&g=34326#2022-11-09
 
-@app.get("/schedule_info")
-async def get_schedule_info():
-    return {
-        "dayOfWeek": "Ср",
-        "typeOfWeek": "Нечет",
-        "date": "09.11.2022",
-    }
+week_day_names = [
+    "mon",
+    "tue",
+    "wed",
+    "thu",
+    "fri",
+    "sat",
+]
+
+types_of_week = [
+    "chet",
+    "nechet"
+]
+
 
 @app.get("/subjects")
-async def get_schedule_info():
-    return [
-        None,
-        {
-            "title": "Основы информационной безопасности",
-            "shortTitle": "ОИБ",
-            "type": "Лекция",
-            "prepod": "Садыков А.М.",
-            "cabinet": "И-1-209",
-            "date": "1 сен - 1 янв"
-        },
-        None,
-        {
-            "title": "Информационные технологии в информационной безопасности",
-            "shortTitle": "ИТВИБ",
-            "type": "Лабораторная работа",
-            "prepod": "Богомолов В.А.",
-            "cabinet": "П-7",
-            "date": "1 сен - 4 дек"
-        },
+async def get_schedule_for_date(date: str):
+    try:
+        date_val = datetime.strptime(date, '%Y-%m-%d')
+    except ValueError:
+        raise HTTPException(status_code=400, detail=f"Bad date: {date}")
 
-        {
-            "title": "Теория вероятностей и математическая статистика",
-            "shortTitle": "ТВИМС",
-            "type": "Практика",
-            "prepod": "Хайруллин М.Х.",
-            "cabinet": "Д-104а",
-            "date": "31 окт - 1 янв"
-        },
-        None,
-        None,
-        None
-    ]
+    num_of_week = date_val.weekday()
+    if num_of_week > 5:
+        raise HTTPException(status_code=400, detail="No sunday in schedule")
 
-@app.get("/full_schedule")
-async def get_schedule_info():
-    return {
-        "dayOfWeek": "Ср",
-        "typeOfWeek": "Нечет",
-        "date": "09.11.2022",
-        "subjects": [
-            None,
-            {
-                "title": "Основы информационной безопасности",
-                "shortTitle": "ОИБ",
-                "type": "Лекция",
-                "prepod": "Садыков А.М.",
-                "cabinet": "И-1-209",
-                "date": "1 сен - 1 янв"
-            },
+    day_of_week_name = week_day_names[num_of_week]
 
-            None,
-            {
-                "title": "Информационные технологии в информационной безопасности",
-                "shortTitle": "ИТВИБ",
-                "type": "Лабораторная работа",
-                "prepod": "Богомолов В.А.",
-                "cabinet": "П-7",
-                "date": "1 сен - 4 дек"
-            },
+    return get_json(chet, day_of_week_name)
 
-            {
-                "title": "Теория вероятностей и математическая статистика",
-                "shortTitle": "ТВИМС",
-                "type": "Практика",
-                "prepod": "Хайруллин М.Х.",
-                "cabinet": "Д-104а",
-                "date": "31 окт - 1 янв"
-            },
-            None,
-            None,
-            None
-        ]
-    }
+
+@app.get("/subjects/{type_of_week}/{day_of_week}")
+async def get_schedule_for_day_of_week(type_of_week: str, day_of_week: str):
+    if day_of_week not in week_day_names or type_of_week not in types_of_week:
+        raise HTTPException(status_code=404, detail="Bad data for day of week")
+
+    return get_json(type_of_week, day_of_week)
+
+
+def get_json(type_of_week: str, day_of_week: str):
+    with open("./api/json/" + type_of_week + day_of_week + ".json", encoding="utf8") as f:
+        return json.load(f)
