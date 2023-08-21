df9f222976ef3ad3e95b677f1e53dc164bd255a2
Hugidonic <netvaca@ya.ru>
2023-08-21 18:51:34 +0300

    fixes

--- api/index.py
+++ api/index.py
@@ -22,27 +22,32 @@ types_of_week = [
     "nechet"
 ]
 
-
-@app.get("/subjects")
+@app.get("/schedule")
 async def get_schedule_for_date(date: str):
     try:
-        date_val = datetime.strptime(date, '%Y-%m-%d')
+        date_val = datetime.strptime(date, '%d.%m.%Y')
     except ValueError:
-        raise HTTPException(status_code=400, detail=f"Bad date: {date}")
+        raise HTTPException(status_code=400, detail={
+            "message": f"Bad date: {date}"
+        })
 
     num_of_week = date_val.weekday()
     if num_of_week > 5:
-        raise HTTPException(status_code=400, detail="No sunday in schedule")
+        raise HTTPException(status_code=400, detail={
+            "message": "No sunday in schedule"
+        })
 
     day_of_week_name = week_day_names[num_of_week]
 
     return get_json("chet", day_of_week_name)
 
 
-@app.get("/subjects/{type_of_week}/{day_of_week}")
+@app.get("/schedule/{type_of_week}/{day_of_week}")
 async def get_schedule_for_day_of_week(type_of_week: str, day_of_week: str):
     if day_of_week not in week_day_names or type_of_week not in types_of_week:
-        raise HTTPException(status_code=404, detail="Bad data")
+        raise HTTPException(status_code=404, detail={
+            "message": f"Bad data"
+        })
 
     return get_json(type_of_week, day_of_week)
 
@@ -50,3 +55,17 @@ async def get_schedule_for_day_of_week(type_of_week: str, day_of_week: str):
 def get_json(type_of_week: str, day_of_week: str):
     with open("./api/json/" + type_of_week + "/" + day_of_week + ".json", encoding="utf8") as f:
         return json.load(f)
+
+
+@app.get("/schedule/details")
+def get_subject_details():
+    return         {
+            "title": "Информационные технологии в информационной безопасности",
+            "shortTitle": "ИТВИБ",
+            "type": "Лабораторная Работа",
+            "prepod": "Богомолов В.А.",
+            "cabinet": "П-7",
+            "duration": "1 сен - 4 дек",
+            "startTime": "9:40",
+            "endTime": "11:10"
+        },
